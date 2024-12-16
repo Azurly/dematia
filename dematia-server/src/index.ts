23a61a4df6eb5b7def01b6e865c5e64f6dd27418
HEMERY Nathan <azerly.hn@gmail.com>
2024-12-16 19:22:45 +0100

    ptn

--- dematia-server/src/index.ts
+++ dematia-server/src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import authRoutes from "./routes/auth";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.use(cors()); // Pour gérer les requêtes CORS
 app.use(express.json()); // Pour parser les requêtes JSON
+
+app.use('/auth', authRoutes);
 
 const PORT = 3002;
 
---
+++ dematia-server/src/routes/auth.ts
@@ -0,0 +1,11 @@
+// src/routes/auth.ts
+
+import { Router } from "express";
+import { login, register } from "../controllers/authController";
+
+const router = Router();
+
+router.post("/login", login);
+router.post("/register", register);
+
+export default router;
