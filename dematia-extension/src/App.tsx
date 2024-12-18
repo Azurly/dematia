72ae94af34aa4d1cf3a5fed50e7368a89e8d50f5
HEMERY Nathan <azerly.hn@gmail.com>
2024-12-18 20:20:27 +0100

    ok

--- dematia-extension/src/App.tsx
+++ dematia-extension/src/App.tsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { motion } from "motion/react"
+import { Bot, Settings, TriangleAlert, User } from 'lucide-react';
+
+// Color for corresponding : #af33f2
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [logged, setLogged] = useState(true);
+  
+  useEffect(() => {
+    const auth = async () => {
+      try {
+        const response = await fetch("",{});
+        if (!response.ok) return null;
+        setLogged(true);
+      }catch(error){
+        return error;
+      }
+    }
+  auth();
+}, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className='container min-w-[250px] h-auto bg-gray-800 p-3 text-white text-center font-semibold'>
+        <nav className='flex justify-between'>
+          <button>
+            <User/>
+          </button>
+          <a className='flex items-center gap-1' href='http://dematia.vercel.app' target='_blank'>
+            <Bot className='text-[#af33f2]'/>
+            <p>DematIA</p>
+          </a>
+          <button>
+            <Settings/>
+          </button>
+        </nav>
+      {logged ?
+      ( <>
+          <div className='flex justify-center items-center'>
+            <TriangleAlert width={50} height={'auto'}/>
+            <p className='text-xl p-4'>Please Sign In</p>
+          </div>
+          <button className='bg-[#af33f2] p-3 rounded-md'>Sign In</button>
+        </>
+      ) : (
+        <section>
+          <div className='p-5 flex justify-center items-center'>
+          <motion.img
+            animate={{scale: 1.2}}
+            transition={{
+              repeat: Infinity, 
+              repeatType: "reverse",
+            }}
+            src='/icon.svg'
+            width={150}
+            height={'auto'}
+            />
+          </div>
+            <p>Welcome, User !</p>
+        </section>
+      )}
+      </main>
     </>
   )
 }
