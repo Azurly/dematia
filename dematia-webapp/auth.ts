2220aceac4bc933160f2beb4e466064b503f742a
HEMERY Nathan <azerly.hn@gmail.com>
2024-12-28 15:12:32 +0100

    adding google

--- dematia-webapp/auth.ts
+++ dematia-webapp/auth.ts
@@ -1,7 +1,46 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import Credentials from "next-auth/providers/credentials"
+import { signInSchema } from "./lib/zod"
+import { ZodError } from "zod"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-  
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        try {
+          const user = null
+          const { email, password } = await signInSchema.parseAsync(credentials)
+          const response = await fetch("", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({email, password}),
+          }
+          );
+
+          if (!response.ok) return null;
+
+          if (!user) {
+            // No user found, so this is their first attempt to login
+            // Optionally, this is also the place you could do a user registration
+            throw new Error("Invalid credentials.")
+          }
+
+          return (await response.json()) ?? null
+        } catch (error) {
+          if (error instanceof ZodError) {
+            // Return `null` to indicate that the credentials are invalid
+            return null
+          }
+        }
+      },
+    }),
+    Google
+  ],
 })
